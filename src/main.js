5736a1a63a81ec1f4f4441d7243e1ad2cf756fb8
Your Name <darreltoledo@icloud.com>
2022-09-25 22:31:16 +0100

    event listener when clicked thed humberger

---
+++ src/main.js
@@ -0,0 +1,6 @@
+let openBtn = document.querySelector('.bi-list');
+let closetBtn = document.querySelector('.bi-x-lg');
+
+openBtn.addEventListener("click",()=>{
+  openBtn.classList.toggle("hide");
+});
