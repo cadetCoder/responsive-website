085e1e54b55d8d8581383434b20136f71f52b779
Your Name <darreltoledo@icloud.com>
2022-09-26 21:52:18 +0100

    close button eventlistener

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 let openBtn = document.querySelector('.bi-list');
-let closetBtn = document.querySelector('.bi-x-lg');
+let closeBtn = document.querySelector('.bi-x-lg');
 
 openBtn.addEventListener("click",()=>{
   openBtn.classList.toggle("hide");
+  closeBtn.classList.toggle("hide");
+});
+
+closeBtn.addEventListener("click",()=>{
+  openBtn.classList.toggle("hide");
+  closeBtn.classList.toggle("hide");
 });
